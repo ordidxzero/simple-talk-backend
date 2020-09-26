a32b23dafd16523a4c1c9ed58accbad44dd6d1ae
kim-macbook <creator@hanyang.ac.kr>
2020-09-26 17:03:35 +0900

    Config userRouter

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,5 +1,27 @@
 import { CtxType } from '../@types';
+import User from '../models/user';
 
-export const test = (ctx: CtxType): void => {
-  ctx.body = 'Success';
+export const loadFriends = async (ctx: CtxType): Promise<void> => {
+  const isAuthenticated = ctx.state.user;
+  if (!isAuthenticated) {
+    ctx.status = 401;
+    return;
+  }
+  const { _id } = isAuthenticated;
+  const user = await User.findById(_id).populate('friends');
+  if (user) {
+    console.log(user.friends);
+  }
+};
+
+export const find = (ctx: CtxType): void => {
+  ctx.body = 'find';
+};
+
+export const addFriend = (ctx: CtxType): void => {
+  ctx.body = 'addFriend';
+};
+
+export const removeFriend = (ctx: CtxType): void => {
+  ctx.body = 'removeFriend';
 };
--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -3,6 +3,9 @@ import routes from '../routes';
 import * as userController from '../controllers/userController';
 const userRouter = new Router();
 
-userRouter.get(routes.user, userController.test);
+userRouter.get(routes.loadFriends, userController.loadFriends);
+userRouter.get(routes.find, userController.find);
+userRouter.get(routes.addFriend(), userController.addFriend);
+userRouter.get(routes.removeFriend(), userController.removeFriend);
 
 export default userRouter;
